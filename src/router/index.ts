18edc77c20b7e578cd43855fb3c064f1505f4adc
Vladislav1992 <kuryanov.vlad@yandex.ru>
2024-04-14 04:31:20 +0300

    recovery password

--- src/router/index.ts
+++ src/router/index.ts
@@ -18,6 +18,14 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import('../pages/AboutView.vue'),
     },
+    {
+      path: '/recovery',
+      name: 'recovery',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../components/RecoveryPassword.vue'),
+    },
   ],
 });
 
